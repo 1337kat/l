3dc3179d8ed0c2f9f835e1f179d45c01800450d4
1337kat <99161297+1337kat@users.noreply.github.com>
2023-09-24 01:19:45 -0700

    Update Keybind hitbox.lua

--- Keybind hitbox.lua
+++ Keybind hitbox.lua
@@ -83,7 +83,7 @@ userInputService.InputBegan:Connect(function(inputObject, gameProcessedEvent)
         settings.headScale = Vector3.new(13, 13, 13) -- Units
         checkHeads() -- Update all heads
     elseif keyCode == Enum.KeyCode.V then -- Keybind
-        settings.headScale = Vector3.new(17, 17, 17) -- Units
+        settings.headScale = Vector3.new(14, 14, 14) -- Units
         checkHeads() -- Update all heads 
     end
 end)
