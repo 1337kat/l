51bc8a68031f199efc7c8fa84a0bf17d7a941509
1337kat <99161297+1337kat@users.noreply.github.com>
2023-10-01 17:01:28 -0700

    Update l.lua

--- l.lua
+++ l.lua
@@ -23,12 +23,7 @@ local Thread4 = coroutine.create(function()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/1337kat/l/main/Chams.lua'))()
 end)
 coroutine.resume(Thread4)
-
---Thread5
-local Thread5 = coroutine.create(function()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
-end)
-coroutine.resume(Thread5)
+--RemovedInfYield
 
 --Thread6
 local Thread6 = coroutine.create(function()
